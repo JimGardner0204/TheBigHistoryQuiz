78ddbf13cdd8457279d8d132489b29cddd4c768f
JimGardner0204 <jpginvoicing@outlook.com>
2023-02-06 19:54:45 +0000

    initial questions added to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+let quizContent = [
+    {
+        question: "Who was the sixth and final wife of Henry VIII?",
+        1: "Anne Boleyn",
+        2: "Catherine of Aragon",
+        3: "Jane Seymour",
+        4: "Catherine Parr",
+        correct: "4",
+    }
+    {
+        question: "What did the Romans call Scotland?",
+        1: "Caledonia",
+        2: "Celtic",
+        3: "Hebrides",
+        4: "Albion",
+        correct: "1",
+    }
+    {
+        question: "In 1918 Finland declared its independence from which country?"
+        1: "Norway",
+        2: "Denmark",
+        3: "Sweden",
+        4: "Russia",
+        correct: "Russia",
+    }
+]
